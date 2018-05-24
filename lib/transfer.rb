2eb171d7c849f800f9446dd7a67cbd7211f16e75
Brent Bauman <brentbauman86@gmail.com>
2018-05-24 14:29:11 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,8 +17,14 @@ class Transfer
   end
 
   def execute_transaction
-    #  binding.pry
-
+    if @sender.balance > @amount && @status == "pending"
+       @sender.balance -= @amount
+       @receiver.balance += @amount
+       @status = "complete"
+    else
+       @status = "rejected"
+       return "Transaction rejected. Please check your account balance."
+    end
   end
 
 
