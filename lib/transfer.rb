a8684a310e565c75bebe4aec96019b5abced2f08
Brent Bauman <brentbauman86@gmail.com>
2018-05-24 14:09:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
+require"pry"
 class Transfer
+
+  attr_accessor :sender, :receiver, :amount
+  attr_reader :status
+
+  def initialize(sender, receiver, amount)
+# binding.pry
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status  = "pending"
+  end
+
+  def valid?
+ # binding.pry
+    if @sender == sender && @receiver == receiver
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    # binding.pry
+
+  end
+
 
 end
